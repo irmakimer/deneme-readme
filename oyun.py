cd058f6252d18edf6efb217061f249924a24980e
Irmak imer <iimer22@posta.pau.edu.tr>
2025-04-25 17:28:21 +0300

    Ekran penceresi oluşturuldu ve renkler isimlendirildi.

--- oyun.py
+++ oyun.py
@@ -6,3 +6,20 @@ import sys
 import random
 import time
 import os
+
+pygame.init()
+
+# Ekran Penceresi
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Basket Oyunu")
+clock = pygame.time.Clock()
+
+# Renkler
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 50, 50)
+GREEN = (0, 200, 0)
+BLUE = (50, 100, 255)
+YELLOW = (255, 255, 0)
+
